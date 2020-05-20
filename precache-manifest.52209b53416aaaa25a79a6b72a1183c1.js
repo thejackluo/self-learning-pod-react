773831fa0bc609218f1cffdc6139f9bfe557c762
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-05-19 17:14:45 -0700

    Updates

---
+++ precache-manifest.52209b53416aaaa25a79a6b72a1183c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cc9b2a5bb274fab25472cfb4531200",
+    "url": "/example-project/index.html"
+  },
+  {
+    "revision": "f1e8951e42ef49fe2035",
+    "url": "/example-project/static/css/main.afa7b907.chunk.css"
+  },
+  {
+    "revision": "e673d180fe293c462d5c",
+    "url": "/example-project/static/js/2.d79b14e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example-project/static/js/2.d79b14e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e8951e42ef49fe2035",
+    "url": "/example-project/static/js/main.8796b19d.chunk.js"
+  },
+  {
+    "revision": "5c7ce6922f29b70fe246",
+    "url": "/example-project/static/js/runtime-main.26834406.js"
+  }
+]);
