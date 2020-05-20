a0e42aef632575db9eca7764f2db44f3c0bc4f3a
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-05-19 17:48:40 -0700

    Update App.js

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -34,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container-fluid">
+          <h1>Hello World</h1>
           <Form handleSubmit={this.handleSubmit} />
           <Table
             characterData={characters}
